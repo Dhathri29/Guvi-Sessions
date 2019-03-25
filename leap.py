34ea33fb14a4e2854a9bdc783cf0ff45a7730911
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-03-25 13:40:13 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,10 +1,9 @@
-def year(n):
-   if (n%4==0) and (n%100!=0) or (n%400==0):
-      print("Yes")
-   else:
-      print("No")
+n=int(input())
+if (n%4==0) and (n%100!=0) or (n%400==0):
+   print("Yes")
+else:
+   print("No")
       
-year(2013)
-year(2048)
+
 
       
