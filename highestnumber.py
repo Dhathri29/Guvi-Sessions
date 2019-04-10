aa98c2e5dc34d5a646e53775c99eb19ce46413f3
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-10 11:13:41 +0530

    Create highestnumber.py

---
+++ highestnumber.py
@@ -0,0 +1,7 @@
+number=int(input())
+num = list(map(int,input().split()))
+num = sorted(num , reverse=1)
+largest_num = 0
+for i in num:
+    largest_num = (largest_num * 10) + i
+print(largest_num)    
