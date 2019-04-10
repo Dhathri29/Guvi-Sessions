1b466948973ed048364a9e45b386a07215a96b2c
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-10 11:23:46 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+n=int(input())
+list1=list(map(int,input().split()))
+c=0
+s=""
+for j in range(0,len(1)):
+    if j==list1[j]:
+          s=s+str(l[j])+""
+          c=c+1
+if c==0:
+    print(-1)
+else:
+    print(s.strip())
+    
