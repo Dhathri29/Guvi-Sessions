14e605811d3ea1ee1fc1d7b9b4ecc27747d7ff3a
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-10 11:26:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ n=int(input())
 list1=list(map(int,input().split()))
 c=0
 s=""
-for j in range(0,len(1)):
+for j in range(0,len(list1)):
     if j==list1[j]:
           s=s+str(l[j])+""
           c=c+1
