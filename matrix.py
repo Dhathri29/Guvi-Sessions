3b180034ee80b88978cc2fc20bffa8f4035b8e47
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-10 19:59:57 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,4 +1,4 @@
-n,k=map(int,input().split()))
+n,k=map(int,input().split())
 m=[]
 for i in range(n):
     li=list(map(int,input().split()))
@@ -16,7 +16,7 @@ for i in i1:
 for i in range(n):
    for j in j1:
        m[i][j]==0
-for i in range(n);
+for i in range(n):
     print(*m[i])
        
     
