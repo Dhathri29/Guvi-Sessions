68cd940b8f8fa10b91c894d85ff443168a0cb188
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-12 07:56:30 +0530

    Update triplets.py

--- triplets.py
+++ triplets.py
@@ -2,8 +2,8 @@ s=int(input())
 n=input().split()
 d=0
 for i in range(0,len(n)):
-    n.insert(i,int(n[i]))
-    n.remove(n[i+1])
+   n.insert(i,int(n[i]))
+   n.remove(n[i+1])
 for i in range(0,len(n)-2):
    for j in range(1,len(n)-1):
       for k in range(2,len(n)):
