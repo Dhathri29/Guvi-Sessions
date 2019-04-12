893dc1b16aaea21613fbb0d9e6953bb3d328aaab
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-12 07:57:49 +0530

    Update triplets.py

--- triplets.py
+++ triplets.py
@@ -7,7 +7,7 @@ for i in range(0,len(n)):
 for i in range(0,len(n)-2):
    for j in range(1,len(n)-1):
       for k in range(2,len(n)):
-         if(n[i]<n[j]<n[k]])and(i<j<k):
+         if(n[i]<n[j]<n[k])and(i<j<k):
             d=d+1
 print(d)            
             
