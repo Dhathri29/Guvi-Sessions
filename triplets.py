a27cae7c3610dcd357736735a65bdc5f1bb43bcc
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-12 07:51:33 +0530

    Update triplets.py

--- triplets.py
+++ triplets.py
@@ -1,13 +1,13 @@
 s=int(input())
-m=input().split()
+n=input().split()
 d=0
-for i in range(0,len(m)):
-    m.insert(i,int(m[i]))
-    m.remove(m[i+1])
-for i in range(0,len(m)-2):
-   for j in range(1,len(m)-1):
-      for k in range(2,len(m)):
-         if(m[i]<m[j]<m[k]])and(i<j<k):
+for i in range(0,len(n)):
+    n.insert(i,int(n[i]))
+    n.remove(n[i+1])
+for i in range(0,len(n)-2):
+   for j in range(1,len(n)-1):
+      for k in range(2,len(n)):
+         if(n[i]<n[j]<n[k]])and(i<j<k):
             d=d+1
 print(d)            
             
