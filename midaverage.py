667f8d801029da41a03291ebf251ea7a7216721b
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-04-12 12:26:11 +0530

    Update midaverage.py

--- midaverage.py
+++ midaverage.py
@@ -1,11 +1,12 @@
-n1=int(input())
+n=int(input())
 li=list(map(int,input().split()))
 left=[]
 right=[]
-for i in range(0,n1-1):
+for i in range(0,n-1):
     left=li[:i+1]
     right=li[i+1:]
     if sum(left)//len(left) == sum(right)//len(right):
         print("yes")
+        break
 else:
    print("no")
