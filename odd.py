e89607877cd2414f6b66827592a62ef18e34ad61
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-02 19:10:45 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,5 +1,5 @@
-s=input
-t=llist(s)
-t[::2],t[1::2]=t[1::2],t[::2]
-''.join(t)
-print(*t,sep="")
+num=input()
+n1=[num[i] for i in range(len(num)) if i%2==0]
+n4=[num[i] for i in range(len(num)) if i%2!=0]
+for j in range(len(num)//2):
+  print(n4[j]+n1[j],end="")
