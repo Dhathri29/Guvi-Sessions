58b88c7ebe979ab3e1bf66bd20e0124671b5cd08
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-03 12:29:28 +0530

    Update index1.py

--- index1.py
+++ index1.py
@@ -1,8 +1,8 @@
 a,b=list(map(int,input().split()))
-c=list(map(int,input().split()))[:a]
+c1=list(map(int,input().split()))[:a]
 d=list(map(int,input().split()))[:b]
 e=list(map(int,input().split()))[:b]
 f=list(map(int,input().split()))[:b]
-print(min(c+d))
-print(min(c+e))
+print(min(c1+d))
+print(min(c1+e))
 print(min(c+f))
