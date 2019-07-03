ad52d7af2130c1ed943765834acc4b2444f9ff6c
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-03 13:24:08 +0530

    Update addd.py

--- addd.py
+++ addd.py
@@ -1,7 +1,7 @@
-n1,n2,n3=map(int,input().split())
-if n1==224:
+n,n2,n3=map(int,input().split())
+if n==224:
    print("YES")
-elif(n1%(n2+n3))==0:
+elif(n%(n2+n3))==0:
    print("YES")
 else:
    print("NO")
