f83ec6b75bbafb8d844e7665d4d5b5f981625a5f
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-05 11:57:34 +0530

    Create XOR_in_sublist.py

---
+++ XOR_in_sublist.py
@@ -0,0 +1,10 @@
+a,b=map(int,input().split())
+l1=[]
+lis=list(map(int,input().split()))
+for i in range(b):
+   u,v=map(int,input().split())
+   y=0
+   for i in range(u-1,v)
+       y=y^lis[i]
+   l1.append(y)
+print(*l1,sep="\n")   
