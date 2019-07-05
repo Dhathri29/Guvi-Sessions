8f96b477af8a4abd21f892ee33973306516ce284
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-05 12:43:08 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,4 +1,10 @@
 a=int(input())
 n=list(map(int,input().split()))
-n.sort(reverse=True)
-print(*n,sep="\n")
+l1=[]
+for i in n:
+  k=bin(i)
+  l1.append(k)
+f=sorted(l1)
+f.reverse()
+for j in f:
+  print(int(j,2))
