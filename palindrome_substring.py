cc42ccc2724247fc65dbd0857218511e789dad6d
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-10 18:40:06 +0530

    Update palindrome_substring.py

--- palindrome_substring.py
+++ palindrome_substring.py
@@ -1,16 +1,16 @@
- def fun()
+def fun()
     n=input()
     x=[]
     x1=""
-    if(n==n[::-]):
+    if(n==n[::-1]):
        x.append(0)
     for i in range(0,len(n)-1):
        for j in range(i+1,len(n)):
           x1=n[i:j+1]
-          if v==v[::-1]:
-              x.append(len(n)-len(v))
+          if x1==x1[::-1]:
+              x.append(len(n)-len(x1))
     print(min(x))
- fun()  
+fun()  
    
          
    
