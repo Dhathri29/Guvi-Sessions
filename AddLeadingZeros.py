47121677c4e568dbe33e387ade26578f496ce944
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-11 19:16:10 +0530

    Create AddLeadingZeros.py

---
+++ AddLeadingZeros.py
@@ -0,0 +1,13 @@
+n=int(input())
+if n==n[::-1]:
+   print("yes")
+elif:
+   a=n.strip('0')
+   if a==a[::-1]:
+      print("yes")
+elif:
+   b=n.lstrip('0')
+   if b==b[::-1]:
+      print("yes")
+else:
+   print("no")
