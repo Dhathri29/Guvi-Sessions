3694850e37c2fa094f2499a80c83a97558771303
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-12 18:20:45 +0530

    Create ShringCandies.py

---
+++ ShringCandies.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=[1]*n
+for i in range(n-1):
+   if(l[i]<l[i+1] and c[i]>=c[i+1]):
+      c[i+1]=c[i]+1
+for i in range(n-1,0,-1):
+   if l[i]<l[i+1] and c[i]>=c[i-1]:
+       c[i-1]=c[i]+1
+print(sum(c))       
