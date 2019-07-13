19ed8a579b0c109d0cb8869d22837b8d200cec70
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-13 10:30:37 +0530

    Update summingUp.py

--- summingUp.py
+++ summingUp.py
@@ -1,10 +1,9 @@
 n,m=map(int,input().split())
 l=list(map(int,input().split()))
 p="no"
-for i in range(len(l));
-    for j in range(l[i+1]:):
+for i in range(len(l)):
+    for j in range l[i+1:]:
        if (l[i]+j)==m:
        p="yes"
        break
 print(p)       
-       
