54ae5032d0572ec580fc3dde656a78e94a299dab
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-13 11:00:33 +0530

    Update lmatrix.py

--- lmatrix.py
+++ lmatrix.py
@@ -5,7 +5,7 @@ for m in range(ni):
     tem.append(list(map(int,input().split()))
 for m in range(ni):
     for j in range(mii-1):
-        for k in range(j+1,mii+1)
+        for k in range(j+1,mii+1):
             if tem[i][j:k]==[1]*len(tem[m][j:k]):
               if all(tem[p+m][j:k][j:k]==[1]*len(tem[p+m][j:k]) for p in range(len(tem[m][j:k])-1)):
                  if len(tem[m][j:k])>x:
