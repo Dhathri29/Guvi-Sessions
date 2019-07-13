284a21eecceec4829ff38c57ea296fd2828240a1
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-07-13 10:58:34 +0530

    Update lmatrix.py

--- lmatrix.py
+++ lmatrix.py
@@ -1,15 +1,15 @@
-n,m=map(int,input().split())
+ni,mii=map(int,input().split())
 tem=[]
 x=0
-for i in range(n):
+for m in range(ni):
     tem.append(list(map(int,input().split()))
-for i in range(n):
-    for j in range(m-1):
-        for k in range(j+1,m+1)
-            if tem[i][j:k]==[1]*len(tem[i][j:k]):
-              if all(tem[p+i][j:k][j:k]==[1]*len(tem[p+i][j:k]) for p in range(len(tem[i][j:k])-1)):
-                 if len(tem[p][j:k])>x:
-                    x=len(tem[i][j:k])
+for m in range(ni):
+    for j in range(mii-1):
+        for k in range(j+1,mii+1)
+            if tem[i][j:k]==[1]*len(tem[m][j:k]):
+              if all(tem[p+m][j:k][j:k]==[1]*len(tem[p+m][j:k]) for p in range(len(tem[m][j:k])-1)):
+                 if len(tem[m][j:k])>x:
+                    x=len(tem[m][j:k])
 if len(tem)==1 and len(tem[0])==1 and tem[0][0]==1:
    print(1)
 for m in range(x):
